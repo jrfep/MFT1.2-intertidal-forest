cd1ae1e3a42db1042f87d7852a4163337834b689
jrfep <jr.ferrer.paris@gmail.com>
2021-08-15 10:51:39 +1000

    approximate values for very large provinces

--- workflow/level4-units/pre-assessment.R
+++ workflow/level4-units/pre-assessment.R
@@ -33,5 +33,60 @@ for (arch in archs) {
                 AOO,AOO_m,EOO) 
   )
 }
-save(file=sprintf("%s/apps/L4map/assmntdata.rda",script.dir),all_rslts)
 
+
+## For the larger missing provinces we will use a shortcut to approximate EOO 
+faltan <- slc.2016[!slc.2016 %in% all_rslts$PROV_CODE]
+
+rslts_approx <- tibble(PROV_CODE=numeric())
+
+# This calculation of AOO is approximate (over-estimating area by ca. 10X) use this breaks/labels:
+aoo.labs <- c("<10","10-50","50-100","100-250","250-500","500-1000",">1000")
+aoo.brks <- c(0,10,50,100,250,500,1000,Inf)
+
+
+for (j in faltan) {
+  for (y in c(1996,2016)) {
+    if (y %in% 2016) {
+      # using the convex hull of the polygons instead of the polygons speeds up the processing
+      mgt.2016.pols %>%  filter(PROV_CODE %in% j) %>% st_convex_hull() -> mgt.slc
+    } else {
+      mgt.1996.pols %>%  filter(PROV_CODE %in% j) %>% st_convex_hull() -> mgt.slc
+    }
+    mgt.slc %>% summarise -> out
+    EOO.all <- st_convex_hull(out)
+    
+    raw.grid <- st_make_grid(EOO.all,cellsize=10000)
+    raw.grid %>%  st_sf(layer = 1:length(raw.grid), geoms = ., stringsAsFactors = FALSE) -> grid
+    
+    data.intersect <- st_intersection(mgt.slc, grid) %>% mutate(area = st_area(.))
+    grid.area <- data.intersect %>% group_by(layer) %>% summarize(area = sum(area))
+    grid %>% mutate(total_area=st_area(geoms)) %>% st_drop_geometry() %>% inner_join(st_drop_geometry(grid.area)) %>% mutate(p.area=(area*100/total_area) %>% drop_units)  -> AOO.all
+    
+    a1 <- mgt.slc %>% ungroup %>% summarise(PROV_CODE=j,year=y,Class='all',Sedimentar='all',area_ogr=sum(set_units(AREA_proj,"m^2") %>% set_units("km^2")),area_poly=sum(st_area(geometry)) %>% set_units('km^2')) %>% st_drop_geometry
+
+    a2 <- AOO.all %>% summarise(PROV_CODE=j,year=y,Class='all',Sedimentar='all',area_grid=sum(area) %>% set_units("m^2") %>% set_units("km^2"),
+                                  AOO=cut(n(),breaks=aoo.brks,labels=aoo.labs))  
+    a3 <- EOO.all %>% ungroup %>% summarise(PROV_CODE=j,year=y,Class='all',Sedimentar='all',EOO=st_area(geometry) %>% set_units('km^2')) %>% st_drop_geometry
+    a1 %>% left_join(a2) %>% left_join(a3)
+    
+    rslts_approx %<>% bind_rows(a1 %>% left_join(a2) %>% left_join(a3))
+  
+    a1 <- mgt.slc %>% group_by(Class,Sedimentar) %>% summarise(area_ogr=sum(set_units(AREA_proj,"m^2") %>% set_units("km^2")),area_poly=sum(st_area(geometry) %>% set_units('km^2'))) %>% st_drop_geometry
+    
+    grid.area <- data.intersect %>% group_by(layer,Class,Sedimentar) %>% summarize(area = sum(area))
+    grid %>% mutate(total_area=st_area(geoms)) %>% st_drop_geometry() %>% inner_join(st_drop_geometry(grid.area)) %>% mutate(p.area=(area*100/total_area) %>% drop_units)  -> AOO.grp
+    a2 <- AOO.grp %>% group_by(Class,Sedimentar) %>% summarise(PROV_CODE=j,year=y,area_grid=sum(area) %>% set_units("m^2") %>% set_units("km^2"),AOO=cut(n(),breaks=aoo.brks,labels=aoo.labs)) 
+    
+    EOO.grp <- mgt.slc %>% group_by(Class,Sedimentar) %>% summarise %>% st_convex_hull %>% mutate(EOO=st_area(geometry) %>% set_units(km^2)) 
+    a3 <- EOO.grp %>% st_drop_geometry()
+    
+    rslts_approx %<>% bind_rows(a1 %>% left_join(a2) %>% left_join(a3))
+  }
+}
+
+rslts_approx
+
+rslts_approx %>% transmute(overest=area_poly/area_ogr) %>% summarise(range(overest))
+
+save(file=sprintf("%s/apps/L4map/assmntdata.rda",script.dir),all_rslts,rslts_approx)
