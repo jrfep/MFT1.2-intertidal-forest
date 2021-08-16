a35e1ac80a2437555de2e19ddec0b3cca1b1e091
jrfep <jr.ferrer.paris@gmail.com>
2021-08-16 13:19:04 +1000

    script to download records from GBIF

--- inc/R/download-from-GBIF.R
+++ inc/R/download-from-GBIF.R
@@ -12,36 +12,43 @@ setwd(work.dir)
 target.dir <- sprintf("%s/species-dist/global/GBIF/%s/spocc/%s", gis.data, projectname, Sys.Date())
 system(sprintf("mkdir -p %s",target.dir))
 
-#Check species associated with mangroves:
-drv <- dbDriver("PostgreSQL") ## remember to update .pgpass file
-con <- dbConnect(drv, dbname = iucn.dbinfo[["database"]],
-                 host = iucn.dbinfo[["host"]],
-                 port = iucn.dbinfo[["port"]],
-                 user = iucn.dbinfo[["user"]])
 
-qry <- "SELECT taxonid,kingdom_name, phylum_name,   class_name, order_name, family_name, genus_name, scientific_name, taxonomic_authority, infra_rank, infra_name, population, category,  main_common_name, code,habitat, suitability, season, majorimportance FROM rlts_spp_taxonomy LEFT JOIN rlts_spp_habitats USING  (taxonid) where habitat like '%Mangr%'"
-mangrove_species <- dbGetQuery(con,qry)
-dbDisconnect(con)
-
-# quick query to know how many records to download for each species
-
-mangrove_species$gbif_records <- NA
-mangrove_species$gbif_xy_records <- NA
-for (j in 1:nrow(mangrove_species)) {
-  cat(sprintf("Species %s: ",j))
-  if (is.na(mangrove_species[j,"gbif_records"])) {
-    tst <- occ(query = mangrove_species$scientific_name[j], from = 'gbif',limit=1)
-    mangrove_species[j,"gbif_records"] <- tst$gbif$meta$found
-    cat(sprintf("/ with %s GBIF records",tst$gbif$meta$found))
+mi.rda <- sprintf('%s/mangrove-species-overview.rda',target.dir)
+if (!file.exists(mi.rda)) {
+  load(mi.rda)
+} else {
+  
+  #Check species associated with mangroves:
+  drv <- dbDriver("PostgreSQL") ## remember to update .pgpass file
+  con <- dbConnect(drv, dbname = iucn.dbinfo[["database"]],
+                   host = iucn.dbinfo[["host"]],
+                   port = iucn.dbinfo[["port"]],
+                   user = iucn.dbinfo[["user"]])
+  
+  qry <- "SELECT taxonid,kingdom_name, phylum_name,   class_name, order_name, family_name, genus_name, scientific_name, taxonomic_authority, infra_rank, infra_name, population, category,  main_common_name, code,habitat, suitability, season, majorimportance FROM rlts_spp_taxonomy LEFT JOIN rlts_spp_habitats USING  (taxonid) where habitat like '%Mangr%'"
+  mangrove_species <- dbGetQuery(con,qry)
+  dbDisconnect(con)
+  
+  # quick query to know how many records to download for each species
+  
+  mangrove_species$gbif_records <- NA
+  mangrove_species$gbif_xy_records <- NA
+  for (j in 1:nrow(mangrove_species)) {
+    cat(sprintf("Species %s: ",j))
+    if (is.na(mangrove_species[j,"gbif_records"])) {
+      tst <- occ(query = mangrove_species$scientific_name[j], from = 'gbif',limit=1)
+      mangrove_species[j,"gbif_records"] <- tst$gbif$meta$found
+      cat(sprintf("/ with %s GBIF records",tst$gbif$meta$found))
+    }
+    if (is.na(mangrove_species[j,"gbif_xy_records"])) {
+      tst <- occ(query = mangrove_species$scientific_name[j], from = 'gbif',limit=1,has_coords = TRUE)
+      mangrove_species[j,"gbif_xy_records"] <- tst$gbif$meta$found
+      cat(sprintf("/ %s records with coordinates",tst$gbif$meta$found))
+    }
+    cat(sprintf("/ %0.2f %% ready\n ",j*100/nrow(mangrove_species)))
   }
-  if (is.na(mangrove_species[j,"gbif_xy_records"])) {
-    tst <- occ(query = mangrove_species$scientific_name[j], from = 'gbif',limit=1,has_coords = TRUE)
-    mangrove_species[j,"gbif_xy_records"] <- tst$gbif$meta$found
-    cat(sprintf("/ %s records with coordinates",tst$gbif$meta$found))
-  }
-  cat(sprintf("/ %0.2f %% ready\n ",j*100/nrow(mangrove_species)))
+  save(file=mi.rda,mangrove_species)
 }
-
 
 # Download data from GBIF according to the amount of records available, 
 # first species with few records:
