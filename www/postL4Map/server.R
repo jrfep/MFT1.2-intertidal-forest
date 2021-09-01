f7c75f20a375150a13ef9b04f91e4d6b26364012
jrfep <jr.ferrer.paris@gmail.com>
2021-09-01 20:59:07 +1000

    update final map

--- www/postL4Map/server.R
+++ www/postL4Map/server.R
@@ -5,24 +5,73 @@ library(dplyr)
 library(sf)
 require(DT)
 require(tidyr)
+library(ggplot2)
+library(units)
+library(ggforce)
+
 
 options(dplyr.summarise.inform = FALSE)
 
 function(input, output, session) {
+  
+  # create a reactive value that will store the click position
+  data_of_click <- reactiveValues(clickedUnit=NULL)
+  
+  observeEvent(input$map_shape_click,{
+    data_of_click$clickedUnit <- input$map_shape_click
+  })
+  
   output$map <- renderLeaflet({
-    pr_labels <- sprintf("PROVINCE:<br/><strong>%s</strong> ",
-                          provs$PROVINCE) %>% lapply(htmltools::HTML)
-    l4_labels <- sprintf("UNIT %s:<br/><strong>%s</strong><br/> Native: %s ", L4u$id, L4u$unit_name, L4u$native) %>% lapply(htmltools::HTML)
+  #  pr_labels <- sprintf("PROVINCE:<br/><strong>%s</strong> ",
+   #                       provs$PROVINCE) %>% lapply(htmltools::HTML)
+    l4_labels <- sprintf("UNIT:<br/><strong>%s</strong><br/> Native: %s ", post_units$unit_name, post_units$native) %>% lapply(htmltools::HTML)
+    #ku_labels <- sprintf("%s", known_mgv$message) %>% lapply(htmltools::HTML)
 
     leaflet() %>%
-        addProviderTiles(providers$CartoDB.Positron) %>%
-         setView(lng = 40, lat = 10, zoom = 2) %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      setView(lng = 40, lat = 10, zoom = 2) %>%
          # addPolygons(data = provs, color = 'yellow',  weight = 2, fillOpacity = 0.15,
         #            highlightOptions = highlightOptions(weight = 2, color = 'black'),
         #            label=pr_labels,
         #            group='Marine provinces')  %>%
-         addPolygons(data = L4u, color = 'red',  weight = 2, fillOpacity = 0.15,label=l4_labels,
+      addMarkers(data=known_mgv,group='Known unmapped occurrences',label=~message) %>%
+      addPolygons(data = post_units, layerId= ~unit_name, color = 'red',  weight = 2, fillOpacity = 0.15,label=l4_labels,
                               highlightOptions = highlightOptions(weight = 2, color = 'black'),
-                              group='Level 4 units')
-      })
+                              group='Level 4 units') %>%
+      leaflet.extras::addHeatmap(data = mgt_points, blur = 20, max = 0.05, radius = 12,group="Heatmap mangrove polygons (2016)") %>%
+      addLayersControl(
+          overlayGroups = c("Level 4 units", "Heatmap mangrove polygons (2016)","Known unmapped occurrences"),
+          options = layersControlOptions(collapsed = FALSE), position = "topright") 
+        })
+  
+  ## test of text output
+  output$uName=renderText({
+    data_of_click$clickedUnit$id
+    })
+  output$description=renderText({
+    post_units %>% filter(unit_name %in% data_of_click$clickedUnit$id) %>% pull(description) -> desc
+    htmltools::HTML(desc)
+  })
+  
+  # Make a barplot 
+  output$barplot=renderPlot({
+    uname = data_of_click$clickedUnit$id
+    if(is.null(uname)){
+      dts <- units_table %>% group_by(Class,Sedimentar) %>% summarise(total=sum(total))
+      if ( input$slcvar=='npols') {
+        ggplot(dts, aes(x=Class,fill=Sedimentar,y=npols)) + geom_col() + labs(title="All regions")
+      } else {
+        ggplot(dts, aes(x=Class,fill=Sedimentar,y=total)) + geom_col() + labs(title="All regions") + ylab("Area")
+      }
+    } else {
+      dts <- units_table %>% filter(unit_name %in% uname)
+      # Make a barplot of area or number depending of the selected input
+      if ( input$slcvar=='npols') {
+        ggplot(dts, aes(x=Class,fill=Sedimentar,y=npols)) + geom_col() + labs(title=uname) + ylab("Number of polygons")
+      } else {
+        ggplot(dts, aes(x=Class,fill=Sedimentar,y=total)) + geom_col() + labs(title=uname) + ylab("Area")
+      }
+    }
+  })  
+  
 }
