3f61c5647844b5f171079b849c239cbfcdeec056
jrfep <jr.ferrer.paris@gmail.com>
2021-09-01 21:03:43 +1000

    link Rdata

--- www/postL4Map/global.R
+++ www/postL4Map/global.R
@@ -4,35 +4,30 @@ require(sf)
 options(dplyr.summarise.inform = FALSE)
 
 
-load("../Rdata/selected-units.rda")
-load("../Rdata/mgt-point-data.rda")
+#load("mapdata.rda")
+#load("assmntdata.rda")
+#load("../Rdata/prov-data.rda")
+#load("../Rdata/mgv-species-occurrence.rda")
+load("selected-units.rda")
+load("mgt-point-data.rda")
 
 
-post_units %<>%
-  st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=0")) 
+provs <- L4units  %>% group_by(PROVINCE,PROV_CODE) %>%
+  st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=0")) %>% summarise(native=max(native))
+for (slc in c("Hawaii","Southern New Zealand","Northern New Zealand","Central Polynesia","Central Polynesia","Marshall, Gilbert and Ellis Islands","Tropical Southwestern Pacific","Southeast Polynesia" )) {
+  g <- provs %>% filter(PROVINCE %in% slc) %>% st_geometry
+  provs$geom[provs$PROVINCE %in% slc] <- (g + c(360,90)) %% c(360) - c(0,90)
+}
 
-slc <- c("Mangroves of Hawaii","Mangroves of New Zealand","Mangroves of Central Polynesia",
-         "Mangroves of Marshall, Gilbert and Ellis Islands","Mangroves of Tropical Southwestern Pacific",
-         "Mangroves of Southeast Polynesia" )
+ L4u <-  L4units  %>% group_by(unit_name) %>%
+   st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=0")) %>% summarise(native=max(native))
+ for (slc in c("Hawaii","New Zealand Mangroves","Central Polynesia","Central Polynesia","Marshall, Gilbert and Ellis Islands","Tropical Southwestern Pacific","Southeast Polynesia" )) {
+   g <- L4u %>% filter(unit_name %in% slc) %>% st_geometry
+   L4u$geom[L4u$unit_name %in% slc] <- (g + c(360,90)) %% c(360) - c(0,90)
+ }
 
+ L4u %<>% mutate(id= sprintf("MFT1.2_G%02d",vctrs::vec_group_id(unit_name)))
 
-mgt_points <- mgt_points %>% filter(unit_name %in% slc) %>% st_shift_longitude() %>% bind_rows({mgt_points %>% filter(!unit_name %in% slc)})
+#plot(provs['PROVINCE'])
 
-post_units <- bind_rows({post_units %>% filter(!unit_name %in% slc)},
-{post_units %>% filter(unit_name %in% slc) %>% st_shift_longitude()})
-
-# L4u %<>% mutate(id= sprintf("MFT1.2_G%02d",vctrs::vec_group_id(unit_name)))
-
-
-vars <- c(
-   "Area" = "total",
-   "Number of polygons" = "npols"
-)
-
-known_mgv <- bind_rows(
-   tibble(lng=-16.526742,lat=19.459096,message="Nouamghar (Cape Timirist) Park National du Banc d'Arguin"),
-   tibble(lng=-65.122,lat=31.280,message='Mangroves are present in Bermuda'),
-   tibble(lng=73.34406,lat=3.11347,message='Mangroves are present in the Maldives (Kathiresan & Rajendran 2005)'),
-   tibble(lng=159.08342,lat=-31.548905,message='Mangroves are present in Lord Howe Island'),
-   tibble(lng=115.6649,lat=-33.3373,message='Mangroves are present in Bunbury (Western Australia)'))
-
+##st_geometry(provs) = (st_geometry(provs) + c(360,90)) %% c(360) - c(0,90)
