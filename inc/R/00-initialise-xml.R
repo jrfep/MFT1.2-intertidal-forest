2c43abdb55a4115968db401ec24464c29702b9d9
jrfep <jr.ferrer.paris@gmail.com>
2021-10-12 23:27:00 +1100

    Create output documents from xml and word templates

--- inc/R/00-initialise-xml.R
+++ inc/R/00-initialise-xml.R
@@ -3,6 +3,7 @@
 require(dplyr)
 require(XML)
 require(sf)
+require(units)
 require(readr)
 
 source(sprintf("%s/proyectos/IUCN-GET/MFT1.2-intertidal-forest/env/project-env.R",Sys.getenv("HOME")))
@@ -21,12 +22,13 @@ threats <- read_csv(file=sprintf("%s/input/threats.csv",script.dir),col_types = 
 ## Initialize document
 post_units %>% st_drop_geometry %>% select(unit_code,unit_name,shortname) -> slc
 
-for (k in 1:nrow(slc)) {
+for (i in 1:nrow(slc)) {#}
+
   doc = newXMLDoc()
-  cdg <- slc$unit_code[k]
-  unit_name <- slc$unit_name[k]
-  short_name <- slc$shortname[k]
-  
+  cdg <- slc$unit_code[i]
+  unit_name <- slc$unit_name[i]
+  short_name <- slc$shortname[i]
+
   top = newXMLNode("Case-Study", doc=doc)
 
   unit_components <- post_units_components %>% filter(unit_code %in% cdg) %>% transmute(qry=sprintf("%s (%s province)",ECOREGION,PROVINCE)) %>% pull %>% paste(collapse=', ')
@@ -39,15 +41,46 @@ for (k in 1:nrow(slc)) {
   } else {
     assoc.spp <- mga_spp_data %>% slice(0)
   }
-  
-  
 
-  short_desc <- sprintf("The '%s' is a regional ecosystem subgroup (level 4 unit of the IUCN Global Ecosystem Typology) that includes intertidal forest and shrublands of the marine ecoregions of %s. The biota is characterised by %01d species of true mangroves and other key plant taxa that provide structure and resources for other mangove-associated taxa. Mangroves in this subgroup have a mapped extent of at least ... km2 in ... countries and are predominantly ... and .... They are threatened by  conversion for urban and tourism development, agriculture and aquaculture, and by increases in frequency of tropical storms. ", unit_name,unit_components,nrow(key.spp))
+  area_qry <- post_units_components %>% filter(unit_code %in% cdg) %>% summarise(min_area=sum(min_area,na.rm=T) %>% drop_units,max_area=sum(max_area,na.rm=T) %>% drop_units)
+
+  mapped_extent <- area_qry %>% transmute(qry=
+    case_when(
+    min_area %in% c(0,NA) ~ "have very limited extent",
+    max_area<10 ~ "have a mapped extent of less than 10 km^2",
+    max_area<100 ~ "have a mapped extent of less than 100 km^2",
+    TRUE ~ sprintf("have a mapped extent between %s and %s km^2",pretty(min_area)[1],pretty(max_area)[2])))
+
+  country.list <- post_units_countries %>% filter(unit_code %in% cdg)
+  country.str <- if (nrow(country.list)==1) {
+    sprintf('restricted to %s',country.list$WB_NAME)
+  } else {
+    sprintf('across %s countries',nrow(country.list))
+    }
+
+  if (cdg %in% units_table$unit_code) {
+    units_table %>% filter(unit_code %in% cdg) %>% group_by(Sedimentar) %>% summarise(p=sum(total) %>% drop_units) -> tt
+    tt[,2] <- tt[,2]*100/sum(tt[,2])
+
+    sedimentar.class <- if_else(nrow(tt)==2, sprintf("with %0.1f %% %s and %0.1f %% %s sedimentars",tt[1,2],tt[1,1],tt[2,2],tt[2,1]),sprintf("with %s sedimentar",tt[1,1]))
+
+    units_table %>% filter(unit_code %in% cdg) %>% group_by(Class) %>% summarise(p=sum(total) %>% drop_units) -> tt
+    tt[,2] <- tt[,2]*100/sum(tt[,2])
+
+
+    type.class <- if_else(tt[which.max(tt$p),1]<100, sprintf("They are classified as predominantly %s (%0.1f %%)", tt[which.max(tt$p),1],tt[which.max(tt$p),2]),sprintf("They are classified as %s",tt[1,1]))
+  } else {
+    type.class <- sedimentar.class <- ""
+  }
+
+  short_desc <- sprintf("The '%s' is a regional ecosystem subgroup (level 4 unit of the IUCN Global Ecosystem Typology) that includes intertidal forest and shrublands of the marine ecoregions of %s. The biota is characterised by %01d species of true mangroves and other key plant taxa that provide structure and resources for other mangove-associated taxa. Mangroves in this subgroup %s %s. %s %s They are threatened by  conversion for urban and tourism development, agriculture and aquaculture, and by increases in frequency of tropical storms. ", unit_name,unit_components,nrow(key.spp),mapped_extent,country.str,type.class,sedimentar.class)
+
 
 
     ## Initialize first level nodes
   ## The assessment target node has 14 children nodes:
   AT.id <- newXMLNode("AT-id",cdg)
+  AT.sname <- newXMLNode("AT-short-name",short_name)
   AT.descriptions <- newXMLNode("AT-descriptions",
                                 children=list(newXMLNode("AT-description", attrs=list(lang="en"),
                                                          short_desc)))
@@ -67,6 +100,13 @@ for (k in 1:nrow(slc)) {
   AT.class <- newXMLNode("Classifications")
   AT.dist <- newXMLNode("Distribution")
   AT.collapse <- newXMLNode("Collapse-definition")
+  AT.references <- newXMLNode("References",children=
+  list(newXMLNode("Reference","Worthington et al. (2020) 2020. A global biophysical typology of mangroves and its relevance for ecosystem structure and deforestation. Sci. Rep. 10, 14652. https://doi.org/10.1038/s41598-020-71194-5"), newXMLNode("Reference","Bishop et al. (2020) "), newXMLNode("Reference","RLTS (2021) Habitat association, species distribution range and taxonomic information from the IUCN Red List of Threatened Species database."), newXMLNode("Reference","GBIF (2021) Species distribution records from the Global Biodiversity Information Facility"),
+       newXMLNode("Reference","Spalding et al. (2007). Marine Ecoregions of the World: a bioregionalization of coast and shelf areas. BioScience 57: 573-583. doi: 10.1641/B570707. Data URL: http://data.unep-wcmc.org/datasets/38"),
+       newXMLNode("Reference","Giri et al. (2011). *Status and distribution of mangrove forests of the world using earth observation satellite data* (version 1.3, updated by UNEP-WCMC). **Global Ecology and Biogeography** 20: 154-159. doi: [10.1111/j.1466-8238.2010.00584.x]."),
+       newXMLNode("Reference",  "Bunting et al. (2018). The Global Mangrove Watch – a New 2010 Global Baseline of Mangrove Extent. Remote Sensing 10(10): 1669. doi: 10.3390/rs1010669.")
+       ))
+
 
   ## Populate nodes
   ### Classification
@@ -91,7 +131,7 @@ for (k in 1:nrow(slc)) {
   ### Characteristic biota
   # For this node we need to add a summary and list of species:
 
-    biota_desc <- sprintf("The biota of '%s' is characterised by the presence of %01d true mangrove and other key plant species. There are at least %02d species of the %s that have been associated with mangrove habitats in the Red List of Threatened Species database and have natural history collection records or observations within the distribution of this unit (GBIF 2021). ", unit_name,nrow(key.spp), nrow(assoc.spp), paste(unique(assoc.spp$class_name),collapse=", "))
+    biota_desc <- sprintf("The biota of '%s' is characterised by the presence of %01d true mangrove and other key plant species (RLTS 2021). There are at least %02d species of the %s that have been associated with mangrove habitats in the Red List of Threatened Species database and have natural history collection records or observations within the distribution of this unit (GBIF 2021). ", unit_name,nrow(key.spp), nrow(assoc.spp), paste(unique(assoc.spp$class_name),collapse=", "))
 
     nThreatened <- {assoc.spp %>% filter(category %in% c("VU","EN","CR")) %>% nrow()}
 
@@ -103,7 +143,7 @@ for (k in 1:nrow(slc)) {
                      categories[tst$category], tst$main_common_name, presence[tst$season])
            },
            sprintf("These include %s threatened species.",nThreatened))
-    
+
 
   newXMLNode("Biota-Summaries",
              children=list(
@@ -114,7 +154,7 @@ for (k in 1:nrow(slc)) {
 
   spp.list <- unique(c(key.spp$binomial,
                 assoc.spp$scientific_name))
-  
+
   taxon.list <- newXMLNode("taxons",parent=AT.biota)
 
   for (taxon in spp.list)
@@ -125,10 +165,21 @@ for (k in 1:nrow(slc)) {
   ### Distribution
   #Here I write some placeholder text based on the profile for the ecosystem functional group.
 
-  country.list <- post_units_countries %>% filter(unit_code %in% cdg)
 
-  dist_desc <- sprintf("The '%s' includes intertidal forest and shrublands of the marine ecoregions of %s, that extent across %s countries and territories in the regions of %s. ", unit_name, unit_components,
-                       nrow(country.list), paste(unique(country.list$REGION_WB),collapse=", "))
+
+
+   if (area_qry$max_area %in% c(NA,0)) {
+     dist_area <- "Mangrove occurrences in this region is limited to small patches and not represented in global mangrove maps."
+   } else{
+     if (area_qry$max_area <10) {
+       dist_area <- "Mangrove extent in this region is less than 10 km^2 (Giri et al. 2011; Bunting et al. 2018)."
+     } else {
+       area_qry %>% transmute(qry=sprintf("Mangrove extent in this region is estimated between %0.0f and %0.0f km^2 (Giri et al. 2011; Bunting et al. 2018).", min_area,max_area)) %>% pull -> dist_area
+     }
+   }
+   dist_desc <- sprintf("The '%s' includes intertidal forest and shrublands of the marine ecoregions of %s, that extent across %s countries and territories in the regions of %s. %s", unit_name, unit_components,
+                        nrow(country.list), paste(unique(country.list$REGION_WB),collapse=", "), dist_area)
+
 
   newXMLNode("Distribution-Summaries",
              children=list(
@@ -159,11 +210,11 @@ for (k in 1:nrow(slc)) {
     # <Region> <Region-classification-system id="" version="0" selected="" assigned-by=""> <Region-classification-element level=""></Region-classification-element> </Region-classification-system> </Region>
 
 ## Abiotic environment
-  
+
   abiotic1 <- "Mangroves are physiologically intolerant of low temperatures, which excludes them from regions where mean air temperature during the coldest months is below 20°C, where the seasonal temperature range exceeds 10°C, or where ground frost occurs. At the latitudinal limits of their distribution mangroves might show plastic ranges with discontinuous or intermittent presence."
-  
+
   abiotic2 <- "Many mangrove soils are low in nutrients, especially nitrogen and phosphorus. Regional distributions are influenced by interactions among landscape position, rainfall, hydrology, sea level, sediment dynamics, subsidence, storm-driven processes, and disturbance by pests and predators. Rainfall and sediment supply from rivers and currents promote mangrove establishment and persistence, while waves and large tidal currents destabilise and erode mangrove substrates, mediating local-scale dynamics in ecosystem distributions. High rainfall reduces salinity stress and increases nutrient loading from adjacent catchments, while tidal flushing also regulates salinity."
-  
+
   abiotic_desc <- if_else(grepl("Temperate",unit_name) | cdg %in% c("MFT1.2_4_MP_12b", "MFT1.2_4_MP_51", "MFT1.2_4_MP_16", "MFT1.2_4_MP_28"),paste(abiotic1,abiotic2),abiotic2)
 
   newXMLNode("Abiotic-Summaries",
@@ -172,7 +223,7 @@ for (k in 1:nrow(slc)) {
                           abiotic_desc,
                           attrs=list(lang="en"))),
              parent=AT.abiotic)
-  
+
   ## Biotic processes
   newXMLNode("Processes-Summaries",
              children=list(newXMLNode("Processes-Summary",
@@ -181,13 +232,12 @@ for (k in 1:nrow(slc)) {
   ## Ecosystem services
   newXMLNode("Services",children=list(newXMLNode("Ecosystem-service")),parent=AT.services)
   newXMLNode("Services-Summaries", children=list(newXMLNode("Services-Summary","These systems are among the most productive coastal environments. These ecosystems are also major blue carbon sinks, incorporating organic matter into sediments and living biomass. They have well-documented ability of  to protect coastal regions from storms. ",attrs=list(lang="en"))),parent=AT.services)
-  ## Finalise and write output
 
   ## Threats
-  
+
   newXMLNode("Threats-Summaries",children=list(newXMLNode("Threats-Summary",attrs=list(lang="en"),"Mangrove deforestation due to aquaculture. Urbanisation and the associated coastal development, over-harvesting, and pollution from domestic, industrial and agricultural land use urbanisation (coastal development). The position of mangrove forests in intertidal areas renders them vulnerable to predicted sea-level rise as a result of climate change. Tropical storms can damage mangrove forests through direct defoliation and destruction of trees, as well as through the mass mortality of animal communities within the ecosystems. ")),parent=AT.threats)
-  
-  
+
+
   for (k in threats %>%  distinct(Name) %>% pull ) {
     this.Threat.class <- newXMLNode("Threat-classification",
                                     attrs=list(id="IUCN", version="3.2", selected="yes",
@@ -216,22 +266,22 @@ for (k in 1:nrow(slc)) {
                ),
                parent=AT.threats)
   }
-  
+
   ## Collapse definition
-  
+
   newXMLNode("Collapse-summaries",children=list(newXMLNode("Collapse-summary",attrs=list(lang="en"))),parent=AT.collapse)
   newXMLNode("Spatial-collapse-definitions",children=list(newXMLNode("Spatial-collapse","Mangroves are structural engineers and possess specialised traits that promote high nitrogen use efficiency and nutrient resorption that influences major processes and functions in this ecosystem. Ecosystem collapse is considered to occur when the tree cover of diagnostic species of true mangroves declines to zero (100% loss).",
                                                                      attrs=list(lang="en"))),parent=AT.collapse)
-  
+
   newXMLNode("Functional-collapse-definitions",children=list(newXMLNode("Functional-collapse","These are highly dynamic systems, with species distributions adjusting to local changes in sediment distribution, tidal regimes, and local inundation and salinity gradients. Processes that disrupt this dynamic can lead to ecosystem collapse. Ecosystem collapse may occur under any of the following: a) climatic conditions (low temperatures) restrict recruitment and survival of diagnostic true mangroves; b) changes in rainfall and river inputs and/or waves and tidal currents destabilise and erode substrates and disrupt recruitment and growth, c) changes in rainfall and tidal flushing change salinity stress and nutrient loadings affecting survival.",
                                                                         attrs=list(lang="en"))),parent=AT.collapse)
-  
-  
-  
+
+
+
   # Aggregate all nodes to the parent node:
 
   AT = newXMLNode("Assessment-Target", attrs=list(date="2021-09-14", `updated-by`="JRFP", status="draft"), parent=top,
-                  children=list(AT.id, AT.descriptions, AT.names, AT.biota, AT.abiotic, AT.biotic, AT.services, AT.threats, AT.actions, AT.research, AT.CEM, AT.class, AT.dist, AT.collapse))
+                  children=list(AT.id, AT.sname, AT.descriptions, AT.names, AT.biota, AT.abiotic, AT.biotic, AT.services, AT.threats, AT.actions, AT.research, AT.CEM, AT.class, AT.dist, AT.collapse,AT.references))
 
 
 
@@ -245,6 +295,7 @@ for (k in 1:nrow(slc)) {
           <?xml-stylesheet type="text/xsl" href="target.xsl"?> ',
           indent = TRUE,
           encoding = "UTF-8")
+
   rm(doc,top)
   gc()
   system(sprintf("xmllint --format %s > tst",output.file))
